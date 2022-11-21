7eeb0b7761a9461c0590abb6b3b882e5666ccfb4
Dustin Lee <leedu@Dustins-MacBook-Air.local>
2022-11-20 23:42:39 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = 3001;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+app.listen(PORT, () => {
+    console.log(`App listening at port: ${PORT}`);
+})
