ef620c2c69a663aa78eceff7dd81436e214dec6b
Dustin Lee <leedu@Dustins-MacBook-Air.local>
2022-11-26 16:20:40 -0500

    reads file from db.json folder

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
-const { json } = require('express');
 const express = require('express');
-const { appendFile } = require('fs');
+const { readFile, writeFile } = require('fs');
 const path = require('path');
 
 const PORT = 3001;
@@ -8,36 +7,45 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.static('public'));
 
+
+//  html routes
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'));
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/notes.html'));
+    res.sendFile(path.join(__dirname, './public/notes.html'));
 });
+
+app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+//  html routes
 
 app.get('/api/notes', (req, res) => {
-    res.sendFile(__dirname, '/public/notes.html')
+    console.log(`${req.method} requested for /notes endpoint`)
+    const dbNotes = readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) throw err;
+        console.log(`${req.method} request to read db.json.`)
+        console.log('inside readFile()', data)
+    });
+    res.json('res.json() : ' + dbNotes)
 });
 
-app.post('/api/notes', (req, res) => {
-    console.log(`${req.method} has been received`);
-    
-    const data = JSON.stringify(req.body);
-    const dbJson = __dirname + '/db/db.json';
 
-    console.log(req.body);
-    console.log(data);
+// app.post('/api/notes', (req, res) => {
+//     console.log(`${req.method} has been received`);
 
+//     const data = req.body;
+//     console.log(data);
 
-    // appendFile(dbJson, data, (err) => {
-    //     if (err) throw err;
-    //     console.log('The file has been saved!');
-    // });
-});
+//     // readFile(dbJson, data, (err) => {
+//     //     if (err) throw err;
+//     //     console.log('The file has been saved!');
+//     // });
+// });
 
 app.listen(PORT, () => {
     console.log(`App listening at port: ${PORT}`);
