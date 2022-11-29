5de735ee23fc900e0e6ad7f13e2e45176fe1df77
Dustin Lee <leedu@Dustins-MacBook-Air.local>
2022-11-29 02:06:12 -0500

    route added to read existing notes on page load

--- server.js
+++ server.js
@@ -1,14 +1,31 @@
 const express = require('express');
 const { readFile, writeFile } = require('fs');
+const dbJson = require('./db/db.json');
 const path = require('path');
 
 const PORT = 3001;
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
 
+//  api routes to GET and POST json file
+app.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './db/db.json'))
+});
+
+app.post('/api/notes', (req, res) => {
+    console.log(`${req.method} has been received`);
+
+    const data = req.body;
+    console.log(data);
+
+    readFile(dbJson, data, (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
+    });
+});
 
 //  html routes
 app.get('/', (req, res) => {
@@ -17,36 +34,41 @@ app.get('/', (req, res) => {
 
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
+
 });
 
-app.get('*', (req,res) => {
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
 });
-//  html routes
 
-app.get('/api/notes', (req, res) => {
-    console.log(`${req.method} requested for /notes endpoint`)
-    const dbNotes = readFile('./db/db.json', 'utf8', (err, data) => {
-        if (err) throw err;
-        console.log(`${req.method} request to read db.json.`)
-        console.log('inside readFile()', data)
-    });
-    res.json('res.json() : ' + dbNotes)
-});
-
-
-// app.post('/api/notes', (req, res) => {
-//     console.log(`${req.method} has been received`);
-
-//     const data = req.body;
-//     console.log(data);
-
-//     // readFile(dbJson, data, (err) => {
-//     //     if (err) throw err;
-//     //     console.log('The file has been saved!');
-//     // });
-// });
 
 app.listen(PORT, () => {
     console.log(`App listening at port: ${PORT}`);
 });
+
+// const dbNotes = readFile('./db/db.json', 'utf8', async (err, data) => {
+//     if (err) throw err;
+//     console.log(`${req.method} request to read db.json.`)
+//     console.log('inside readFile()', data);
+//     });
+
+// console.log(`${req.method} requested for /notes endpoint`)
+// try {
+//     const dbNotes = readFile('./db/db.json', 'utf8', (err, data) => {
+//         if (err) throw err;
+//         console.log(`${req.method} request to read db.json.`)
+//         console.log('inside readFile()', data)
+//         console.log('readFile return value: ', dbNotes)
+//     })
+// } catch (err) {
+//     console.error(err.message);
+// }
+
+// import { readFile } from 'node:fs/promises';
+// try {
+//     const filePath = new URL('./package.json', import.meta.url);
+//     const contents = await readFile(filePath, { encoding: 'utf8' });
+//     console.log(contents);
+// } catch (err) {
+//     console.error(err.message);
+// }
