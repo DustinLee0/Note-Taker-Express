65d34771acf321142ce57594ea1409f39f33b57e
Dustin Lee <leedu@Dustins-MacBook-Air.local>
2022-12-14 23:25:40 -0500

    port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const { readFile, writeFile } = require('fs');
 const { v4: uuidv4 } = require('uuid')
 const path = require('path');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.json());
